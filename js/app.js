41cd8a346f5ba0d2401847bd8ff4ca0abfc3feae
dev_nayan <md.nayan8494@gmail.com>
2022-07-18 00:53:53 +0600

    Some Modification

--- js/app.js
+++ js/app.js
@@ -7,22 +7,25 @@ const navbarLink = document.getElementById('navbarLink');
 
 window.addEventListener('scroll', () => {
     const scroll = window.scrollY;
-    if(scroll > 150) {
-        navbar.classList.add('fixed-top');
-        navbar.classList.add('py-4');
-        navbar.style.boxShadow = '0 0 10px 5px rgba(0,0,0,0.1)';
-        topNav.classList.add('d-none');
-        navbarBrand.classList.remove('d-none');
-        navbarSocial.classList.add('d-none');
-        navbarLink.classList.add('ms-auto');
-
-    } else {
-        navbar.classList.remove('fixed-top');
-        navbar.classList.remove('py-4');
-        navbar.style.boxShadow = 'none';
-        topNav.classList.remove('d-none');
-        navbarBrand.classList.add('d-none');
-        navbarSocial.classList.remove('d-none');
-        navbarLink.classList.remove('ms-auto');
+    if (window.innerWidth > 1018) {
+        if(scroll > 150) {
+            navbar.classList.add('fixed-top');
+            navbar.classList.add('py-4');
+            navbar.style.boxShadow = '0 0 10px 5px rgba(0,0,0,0.1)';
+            topNav.classList.add('d-none');
+            navbarBrand.classList.remove('d-lg-none');
+            navbarSocial.classList.add('d-lg-none');
+            navbarLink.classList.add('ms-auto');
+    
+        } else {
+            navbar.classList.remove('fixed-top');
+            navbar.classList.remove('py-4');
+            navbar.style.boxShadow = 'none';
+            topNav.classList.remove('d-none');
+            navbarBrand.classList.add('d-lg-none');
+            navbarSocial.classList.remove('d-lg-none');
+            navbarLink.classList.remove('ms-auto');
+        }
     }
+    
 })
