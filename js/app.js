89d94b808b991c0a43b0299c5b8c7dcb6fae1237
dev_nayan <md.nayan8494@gmail.com>
2022-07-19 11:52:28 +0600

    ToTop Button added

--- js/app.js
+++ js/app.js
@@ -1,12 +1,19 @@
-// Navbar JS
+// Variables declaration
 const navbar = document.getElementById('navbar');
 const topNav = document.getElementById('topNav');
 const navbarBrand = document.getElementById('navbarBrand');
 const navbarSocial = document.getElementById('navbarSocial');
+const navbarNav = document.getElementById('navbarNav');
 const navbarLink = document.getElementById('navbarLink');
+const navbarToggler = document.getElementById('navbarToggler');
+const navLinks = document.querySelectorAll('.nav-link');
+const toTop = document.getElementById('toTop');
 
+// Scroll Event
 window.addEventListener('scroll', () => {
     const scroll = window.scrollY;
+
+    // Navbar
     if (window.innerWidth > 1018) {
         if(scroll > 150) {
             navbar.classList.add('fixed-top');
@@ -25,5 +32,28 @@ window.addEventListener('scroll', () => {
             navbarLink.classList.remove('ms-auto');
         }
     }
-    
+
+    // To Top Button
+    if (scroll > 500) {
+        toTop.style.bottom = '2rem';
+    } else {
+        toTop.style.bottom = '-10rem';
+    }
+});
+
+// Nav Link Click Event
+navLinks.forEach(navLink => {
+    navLink.addEventListener('click', () => {
+        navbarToggler.classList.add('collapsed');
+        navbarNav.classList.remove('show');
+        navbarToggler.setAttribute('aria-expanded', 'false');
+    })
+})
+
+// To Top Button Click Event
+toTop.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
 })
